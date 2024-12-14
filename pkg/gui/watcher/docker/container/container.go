978cfd3b500c2c53629417f24051f26e3dcb8397
shoggothforever <1337231450@qq.com>
2024-12-14 14:18:39 +0800

    remove annotation

--- pkg/gui/watcher/docker/container/container.go
+++ pkg/gui/watcher/docker/container/container.go
@@ -19,19 +19,8 @@ var tabUIButtonFuncMap = map[string]func() fyne.CanvasObject{}
 func Screen(w fyne.Window) fyne.CanvasObject {
 	// 动态bpf日志区域
 	bpfLogs := component.NewLogBoard("Real-Time BpfLogs", 600, 200)
-	//bpfLogs.ShowLineNumbers = true
-	//bpfLogs.SetText("Real-Time BpfLogs")
-	////bpfLogs.
-	//bpfLogsScroll := container.NewScroll(bpfLogs)
-	//bpfLogsScroll.SetMinSize(fyne.NewSize(600, 200)) // 限制宽度为 600，高度为 200
 	// image 日志
 	containerLogs := component.NewLogBoard("Container-Creating Logs", 600, 200)
-	//containerLogs.ShowLineNumbers = true
-	//containerLogs.SetText("Container-Creating Logs")
-	//containerScroll := container.NewScroll(containerLogs)
-	//containerScroll.SetMinSize(fyne.NewSize(600, 200)) // 限制宽度为 600，高度为 200
-
-	//var bpfChoices
 	toolbar := NewContainerToolBar(containerLogs, bpfLogs)
 	content := container.NewHBox(
 		toolbar,
